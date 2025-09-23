65b5adac19a411f024a77a727e50e2e6fa65672a
EmonHossen10 <emonhossen1000@gmail.com>
2025-09-23 21:35:19 +0600

    c programme variable anbd data types addded

---
+++ Variable-dataTypes.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main()
+{
+    // variable types
+    int a = 10;
+    float b = 20.5;
+    char c = 'A';
+    double d = 30.99;
+
+    printf("Integer: %d\n", a);
+    printf("Float: %f\n", b);
+    printf("Character: %c\n", c);
+    printf("Double: %lf\n", d);
+    // update variable values
+    a = 15;
+    b = 25.5;
+    c = 'B';
+    d = 35.99;
+    printf("********************************\n");
+    printf("Updated Integer: %d\n", a);
+    printf("Updated Float: %.2f\n", b);
+    printf("Updated Character: %c\n", c);
+    printf("Updated Double: %.2lf\n", d);
+
+    // variable data types and their sizes
+    printf("********************************\n");
+    printf("Size of int: %zu bytes\n", sizeof(a));    // 4 bytes
+    printf("Size of float: %zu bytes\n", sizeof(b));  // 4 bytes
+    printf("Size of char: %zu bytes\n", sizeof(c));   // 1 byte
+    printf("Size of double: %zu bytes\n", sizeof(d)); // 8 bytes
+
+    // boolean and string types doesn't exist in C
+
+    return 0;
+}
